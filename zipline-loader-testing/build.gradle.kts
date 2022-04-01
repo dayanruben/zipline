685306a69f6d301b5020b4e99a3b5161312abf5e
Andrew Alexander <adrw@squareup.com>
2022-04-01 09:41:33 -0700

    Revert "WIP fix build failure"
    This reverts commit f6aada38af17d2758ee3a83c953763d9be4113b1.

--- zipline-loader-testing/build.gradle.kts
+++ zipline-loader-testing/build.gradle.kts
@@ -10,36 +10,35 @@ plugins {
 
 kotlin {
   jvm()
-  //
-  // js {
-  //   browser()
-  //   // TODO upstream this to ZiplinePlugin
-  //   binaries.library()
-  //   binaries.executable()
-  // }
-  //
-  // linuxX64()
-  // macosX64()
-  // macosArm64()
-  // iosArm64()
-  // iosX64()
-  // iosSimulatorArm64()
-  // tvosArm64()
-  // tvosSimulatorArm64()
-  // tvosX64()
+
+  js {
+    browser()
+    // TODO upstream this to ZiplinePlugin
+    binaries.library()
+    binaries.executable()
+  }
+
+  linuxX64()
+  macosX64()
+  macosArm64()
+  iosArm64()
+  iosX64()
+  iosSimulatorArm64()
+  tvosArm64()
+  tvosSimulatorArm64()
+  tvosX64()
 
   sourceSets {
     val commonMain by getting {
-
-    }
-
-    val engineMain by creating {
-      dependsOn(commonMain)
       dependencies {
         implementation(projects.zipline)
         implementation(projects.ziplineLoader)
         implementation(Dependencies.kotlinxSerializationJson)
       }
+    }
+
+    val engineMain by creating {
+      dependsOn(commonMain)
     }
 
     val jvmMain by getting {
@@ -52,20 +51,20 @@ kotlin {
     }
   }
 
-  // targets.all {
-  //   compilations.all {
-  //     val pluginDependency = if (this is AbstractKotlinNativeCompilation) {
-  //       projects.ziplineKotlinPluginHosted
-  //     } else {
-  //       projects.ziplineKotlinPlugin
-  //     }
-  //     // Naming logic from https://github.com/JetBrains/kotlin/blob/a0e6fb03f0288f0bff12be80c402d8a62b5b045a/libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinTargetConfigurator.kt#L519-L520
-  //     val pluginConfigurationName = PLUGIN_CLASSPATH_CONFIGURATION_NAME +
-  //       target.disambiguationClassifier.orEmpty().capitalize() +
-  //       compilationName.capitalize()
-  //     project.dependencies.add(pluginConfigurationName, pluginDependency)
-  //   }
-  // }
+  targets.all {
+    compilations.all {
+      val pluginDependency = if (this is AbstractKotlinNativeCompilation) {
+        projects.ziplineKotlinPluginHosted
+      } else {
+        projects.ziplineKotlinPlugin
+      }
+      // Naming logic from https://github.com/JetBrains/kotlin/blob/a0e6fb03f0288f0bff12be80c402d8a62b5b045a/libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinTargetConfigurator.kt#L519-L520
+      val pluginConfigurationName = PLUGIN_CLASSPATH_CONFIGURATION_NAME +
+        target.disambiguationClassifier.orEmpty().capitalize() +
+        compilationName.capitalize()
+      project.dependencies.add(pluginConfigurationName, pluginDependency)
+    }
+  }
 }
 
 // TODO upstream this to ZiplinePlugin
